9586abb8d044246f0949b0b3b37edd118184b868
murraycopps <murraycopps@oldrochester.org>
2023-01-31 10:07:57 -0500

    conected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,49 @@
-export default function IndexPage({apiKey} : {apiKey: string}) {
-  // Set the request parameters
-  const params = {
-    model: 'text-davinci-002',
-    prompt: `Please generate a weekly training plan for a runner who is preparing for a ${targetRace} race with an experience level of ${experience} and an age of ${age}. That normally runs about ${normalMilage} miles per week and I like to include ${workouts} in my training. The total milage should not be more then 20% off of ${normalMilage} The training plan should be a list of days Monday-Sunday that include a mix of easy and faster-paced runs, and to specify the type of run for each day. When a workout is included, please specify the workout in the following format: "Monday: track workout: 4x400 race pace: 1" (1 represents the miles, make sure to include total milage including warmup and cooldown) but when it isn't a workout (this includes long runs) please have it in the following format “Monday: 11 miles easy”. ${savedDays ? ("The plan already includes: " + savedDays.map((savedDay) => savedDay.day.originalText).join('\n')) : ''}`,
-    max_tokens: 2048,
-};
+import axios from "axios";
+import {FormEvent, useState} from "react";
+
+export default function IndexPage({apiKey}: { apiKey: string }) {
+    const [joke, setJoke] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setLoading(true)
+        // Set the request parameters
+        const params = {
+            model: 'text-davinci-002',
+            prompt: 'tell me a joke',
+            max_tokens: 2048,
+        };
 
 // Make the request
-const response = await axios.post('https://api.openai.com/v1/completions', params, {
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-    },
-});
-  return <h1>IndexPage</h1>;
+        const response = await axios.post('https://api.openai.com/v1/completions', params, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`,
+            },
+        })
+        setJoke(response.data.choices[0].text)
+        setLoading(false)
+    }
+    return (
+        <div className="text-white w-screen flex flex-col items-center p-8">
+            {loading ? <p>Loading...</p> : <>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center">
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Tell me a joke
+                    </button>
+                </form>
+                <div>
+                    {joke}
+                </div>
+            </>}
+        </div>
+    )
 }
+
 export function getStaticProps() {
-  return {
-    props: {
-      apiKey: process.env.OPENAI_API_KEY,
-    },
-  };
+    return {
+        props: {
+            apiKey: process.env.OPENAI_API_KEY,
+        },
+    };
 }
