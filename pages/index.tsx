5124f030bf22b3d2ca9b415505ea2ffd1dda647c
murraycopps <murraycopps@oldrochester.org>
2023-01-31 09:54:10 -0500

    something

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-export default function IndexPage() {
+export default function IndexPage({apiKey} : {apiKey: string}) {
   return <h1>IndexPage</h1>;
+}
+export function getStaticProps() {
+  return {
+    props: {
+      apiKey: process.env.OPENAI_API_KEY,
+    },
+  };
 }
